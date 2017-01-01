37c12a206cd68d5b826e0ee89e61af238bbd29bf
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-01-01 14:52:43 -0600

    :c

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1026,21 +1026,6 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 
 	/* Continue a partial initialization */
 	if (type == HUB_INIT2 || type == HUB_INIT3) {
-<<<<<<< HEAD
-                device_lock(hub->intfdev);
-
-                /* Was the hub disconnected while we were waiting? */
-                if (hub->disconnected) {
-                        device_unlock(hub->intfdev);
-                        kref_put(&hub->kref, hub_release);
-                        return;
-		}
-                if (type == HUB_INIT2)
-                        goto init2;
-		goto init3;
-	}
-        kref_get(&hub->kref);
-=======
 		device_lock(hub->intfdev);
 
 		/* Was the hub disconnected while we were waiting? */
@@ -1054,7 +1039,6 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 		goto init3;
 	}
 	kref_get(&hub->kref);
->>>>>>> 6bd6246... Update the Linux Base Version (3.10.102--->3.10.103)
 
 	/* The superspeed hub except for root hub has to use Hub Depth
 	 * value as an offset into the route string to locate the bits
@@ -1260,11 +1244,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func3);
 			schedule_delayed_work(&hub->init_work,
 					msecs_to_jiffies(delay));
-<<<<<<< HEAD
-                        device_unlock(hub->intfdev);
-=======
 			device_unlock(hub->intfdev);
->>>>>>> 6bd6246... Update the Linux Base Version (3.10.102--->3.10.103)
 			return;		/* Continues at init3: below */
 		} else {
 			msleep(delay);
